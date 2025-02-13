3c5c4f4c612f1507838eecc7ed169406875be55f
r-yin <ryin2@bu.edu>
2025-02-13 17:16:45 -0500

    fix bug and run all tests in series

--- MotorController/MCTets_jetson.py
+++ MotorController/MCTets_jetson.py
@@ -1,14 +1,8 @@
-#RUN BEFORE:
-#sudo apt-get update
-#sudo apt-get install python3-pip
-#sudo pip3 install Jetson.GPIO
-
 import Jetson.GPIO as GPIO
 import time
-import keyboard
 
-#pin definition
-enA = 32 #PWM
+# Pin definitions
+enA = 32  # PWM pin
 in1 = 31
 in2 = 7
 EncoderA = 15
@@ -16,184 +10,96 @@ EncoderB = 33
 
 position = 0
 
+# Set up GPIO mode
 GPIO.setmode(GPIO.BOARD)
 
-pwm = GPIO.PWM(enA, 1000)  # Set PWM frequency to 1kHz
+# Setup motor control pins
+GPIO.setup(in1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(in2, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(enA, GPIO.OUT)
+
+# Initialize PWM on enA
+pwm = GPIO.PWM(enA, 1000)  # Set frequency to 1kHz
 pwm.start(0)  # Start with 0% duty cycle
 
-# Setup GPIO pin as output
-GPIO.setup(enA, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(in1, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(enA, GPIO.OUT, initial=GPIO.LOW)
+# Set up Encoder pins (Remove pull-up setting)
+GPIO.setup(EncoderA, GPIO.IN)
+GPIO.setup(EncoderB, GPIO.IN)
 
-# Set up GPIO
-GPIO.setup(EncoderA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(EncoderB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
+# Encoder callback function
 def encoder_callback(channel):
     global position
-    if GPIO.input(EncoderA) == GPIO.input(EncoderB):
+    A = GPIO.input(EncoderA)
+    B = GPIO.input(EncoderB)
+    
+    if A == B:
         position += 1  # Clockwise
     else:
-        position -= 1  # Counterclockwise   
+        position -= 1  # Counterclockwise
 
 # Attach interrupt to count pulses
 GPIO.add_event_detect(EncoderA, GPIO.BOTH, callback=encoder_callback)
 
-
+# Motor control functions
 def fmotor_control(speed):
+    """Move forward at specified speed (0-100%)"""
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
-    pwm.ChangeDutyCycle(speed)  # Adjust speed (0-100%)
+    pwm.ChangeDutyCycle(speed)
 
 def stop_motor():
+    """Stop motor"""
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.LOW)
-
-def fmotor_full():
-    GPIO.output(in1,GPIO.HIGH)
-    GPIO.output(in2,GPIO.LOW)
-    pwm.ChangeDutyCycle(100)
+    pwm.ChangeDutyCycle(0)
 
 def rmotor_control(speed):
+    """Move in reverse at specified speed (0-100%)"""
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.HIGH)
-    pwm.ChangeDutyCycle(speed)  # Adjust speed (0-100%)
+    pwm.ChangeDutyCycle(speed)
 
-def rmotor_full():
-    GPIO.output(in1,GPIO.LOW)
-    GPIO.output(in2,GPIO.HIGH)
-    pwm.ChangeDutyCycle(100)
-
-def testReversible():
-    #move motor
+def test_reversible():
+    """Test bidirectional motion"""
     fmotor_control(50)
     time.sleep(1)
-    #stop
     stop_motor()
-    time.sleep(.2)
-    #move motor in opposite direction
+    time.sleep(0.2)
     rmotor_control(50)
     time.sleep(1)
-    #stop 
     stop_motor()
     time.sleep(2)
 
-def testVariableSpeed():
-    #move motor
-    fmotor_control(10)
-    print("10%")
-    time.sleep(2)
-
-    fmotor_control(25)
-    print("25%")
-    time.sleep(2)
-
-    fmotor_control(50)
-    print("50")
-    time.sleep(2)
-
-    fmotor_control(75)
-    print("75")
-    time.sleep(2)
-
-    fmotor_control(100)
-    print("100")
-    time.sleep(2)
-    
+def test_variable_speed():
+    """Test increasing speed in steps"""
+    for speed in [10, 25, 50, 75, 100]:
+        fmotor_control(speed)
+        print(f"Speed: {speed}%")
+        time.sleep(2)
     stop_motor()
 
-def testMaxSpeed():
-    fmotor_full()
-    time.sleep(2)
+def test_encoder_feedback():
+    """Test motor movement using encoder feedback"""
+    global position
+    position = 0  # Reset position
+    fmotor_control(20)
+    while position < 100:  # Move until 100 counts
+        pass
     stop_motor()
+    time.sleep(1)
 
-def testReverseSpeed():
-    #move motor
-    fmotor_control(10)
-    print("10%")
-    time.sleep(2)
-
-    stop_motor()
-    time.sleep(.2)
-
-    rmotor_control(10)
-    print("10%")
-    time.sleep(2)
-
-    ############
-
-    fmotor_control(25)
-    print("25%")
-    time.sleep(2)
-
-    stop_motor()
-    time.sleep(.2)
-
-    rmotor_control(25)
-    print("25%")
-    time.sleep(2)
-
-    ############
-
-    fmotor_control(50)
-    print("50")
-    time.sleep(2)
-    
-    stop_motor()
-    time.sleep(.2)
-
-    rmotor_control(50)
-    print("50")
-    time.sleep(2)
-
-    ################
-
-    fmotor_control(100)
-    print("100")
-    time.sleep(2)
-    
-    stop_motor()
-    time.sleep(.2)
-
-    rmotor_control(100)
-    print("100")
-    time.sleep(2)
-
-
-def testEncoderFeedback():
-    while position < 100:
-        fmotor_control(20)
-    rmotor_control(20)
-    time.sleep(10)
-
-#function Map
-function_map = {
-    '1':testReversible,
-    '2':testVariableSpeed,
-    '3':testMaxSpeed,
-    '4':testReverseSpeed,
-    '5':testEncoderFeedback
-}
-
-print("Press a number key (1-5) to execute a function. Press 'q' to quit.")
-
-
-try:
-    while True:
-        key_event = keyboard.read_event()  # Read a key event
-        
-        if key_event.event_type == keyboard.KEY_DOWN:  # Detect key press
-            key = key_event.name
-            
-            if key in function_map:  # Check if the key is mapped
-                function_map[key]()  # Execute the mapped function
-            
-            elif key == 'q':  # Quit condition
-                print("Exiting...")
-                break
-
-except KeyboardInterrupt:
+# Cleanup function
+def cleanup():
+    print("Cleaning up GPIO...")
     GPIO.cleanup()
-    print("Program Exited")
+
+# Main function
+if __name__ == "__main__":
+    try:
+        test_reversible()
+        test_variable_speed()
+        test_encoder_feedback()
+    except KeyboardInterrupt:
+        print("Interrupted by user")
+    finally:
+        cleanup()
