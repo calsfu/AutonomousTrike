be7f5a0ea07df405c6e13ad43b0ae8154f726795
LindenAdamson <98446994+LindenAdamson@users.noreply.github.com>
2025-05-01 03:18:13 -0400

    Update emergency_brake.py

--- SCA/emergency_brake.py
+++ SCA/emergency_brake.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Emergency_Brake:
-    def __init__(self, noise_thresh, kblur, safe_cm, boxes, box_ring):
+    def __init__(self, noise_thresh, kblur, safe_cm, boxes, box_ring, show):
         self.noise_thresh = noise_thresh
         self.kblur = kblur
         self.safe_cm = safe_cm
         self.boxes = boxes
         self.box_ring = box_ring
+        self.show = show
         
     def get_brake(self, npimg):
         datatype = npimg.dtype
@@ -29,6 +31,17 @@ class Emergency_Brake:
         box_avgs = box_avgs.astype(datatype)
         box_avgs = box_avgs[self.box_ring:-self.box_ring, self.box_ring:-self.box_ring]
         box_avgs = np.where(box_avgs == 0, dmax, box_avgs)
+        # if self.show:
+        #     print(box_avgs)
+        #     plt.figure()
+        #     plt.subplot(1,3,1)
+        #     plt.imshow(npimg)
+        #     plt.subplot(1,3,2)
+        #     plt.imshow(edge)
+        #     plt.subplot(1,3,3)
+        #     plt.imshow(cleaned_img_np)
+        #     plt.savefig("figs/eb.jpg", dpi=100)
+        #     plt.close()
         # publishing value
         if(np.min(box_avgs) < self.safe_cm):
             return 1
