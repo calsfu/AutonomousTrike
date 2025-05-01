2901eead81150a9e04627b0ef1122bcc4c33c36d
LindenAdamson <98446994+LindenAdamson@users.noreply.github.com>
2025-05-01 13:50:14 -0400

    Update emergency_brake.py

--- SCA/emergency_brake.py
+++ SCA/emergency_brake.py
@@ -4,22 +4,48 @@ import matplotlib.pyplot as plt
 
 class Emergency_Brake:
     def __init__(self, noise_thresh, kblur, safe_cm, boxes, box_ring, show):
-        self.noise_thresh = noise_thresh
-        self.kblur = kblur
+        # self.noise_thresh = noise_thresh
+        # self.kblur = kblur
         self.safe_cm = safe_cm
-        self.boxes = boxes
-        self.box_ring = box_ring
-        self.show = show
+        # self.boxes = boxes
+        # self.box_ring = box_ring
+        # self.show = show
         
-    def get_brake(self, npimg):
+        self.noise_thresh = 1000
+        self.kblur = 19
+        # self.safe_cm = 3000
+        self.boxes = 6
+        self.box_ring = 1 # can't be > boxes / 2
+
+    def get_brake(self, npimg_in):
+        npimg = np.copy(npimg_in).astype(np.uint16)
         datatype = npimg.dtype
         dmax = np.iinfo(datatype).max
         # image processing
         edge = cv2.Sobel(src=npimg, ddepth=cv2.CV_16UC1, dx=1, dy=1, ksize=7)
+        # edge = np.asarray(edge_out, dtype=float)
+        
+        # plt.figure()
+        # plt.subplot(1,2,1)
+        # plt.imshow(npimg)
+        # plt.subplot(1,2,2)
+        # plt.imshow(edge)
+        # plt.savefig("figs/eb.jpg", dpi=100)
+        # plt.close()
+
         ksize = (self.kblur, self.kblur) 
         blur = cv2.blur(edge, ksize)  
+        # print(blur_out)
+        # blur = np.asarray(blur_out)
+        # print(blur_out.dtype)
+        # blur = cv2.GaussianBlur(edge, (19, 19), 0)
+        # blur = cv2.medianBlur(edge, 5)
         cleaned_img_zeros = np.where(blur < self.noise_thresh, npimg, 0)
+        # cleaned_img_zeros = np.where(blur_out < 1000, npimg, 0)
         cleaned_img_np = np.where(cleaned_img_zeros == 0, np.nan, npimg)
+        # except:
+        #     print("blur broke")
+        #     return
         # finding nearest object
         x, y = cleaned_img_np.shape
         x, y = int(x / self.boxes), int(y / self.boxes)
